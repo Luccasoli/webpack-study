c4a3e1f804d80c130e82e914612e067f403e5c16
Lucas <lucasoliveira98@outlook.com>
2021-07-18 18:57:18 -0300

    ✨ [Caching] extracting third-party libraries to a separate vendor chunk

---
+++ dist/index.da52bdc9e070da0dddec.bundle.js
@@ -0,0 +1,36 @@
+(self["webpackChunkwebpack_demo"] = self["webpackChunkwebpack_demo"] || []).push([["index"],{
+
+/***/ "./src/index.js":
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var lodash__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! lodash */ "./node_modules/lodash/lodash.js");
+/* harmony import */ var lodash__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(lodash__WEBPACK_IMPORTED_MODULE_0__);
+
+
+function component() {
+	const element = document.createElement('div');
+
+	// Lodash, now imported by this script
+	element.innerHTML = lodash__WEBPACK_IMPORTED_MODULE_0___default().join(['Hello', 'webpack'], ' ');
+
+	return element;
+}
+
+
+/***/ })
+
+},
+/******/ __webpack_require__ => { // webpackRuntimeModules
+/******/ "use strict";
+/******/ 
+/******/ var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
+/******/ __webpack_require__.O(0, ["vendors"], () => (__webpack_exec__("./src/index.js")));
+/******/ var __webpack_exports__ = __webpack_require__.O();
+/******/ }
+]);
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly93ZWJwYWNrLWRlbW8vLi9zcmMvaW5kZXguanMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6Ijs7Ozs7Ozs7Ozs7O0FBQXVCOztBQUV2QjtBQUNBOztBQUVBO0FBQ0EscUJBQXFCLGtEQUFNOztBQUUzQjtBQUNBIiwiZmlsZSI6ImluZGV4LmRhNTJiZGM5ZTA3MGRhMGRkZGVjLmJ1bmRsZS5qcyIsInNvdXJjZXNDb250ZW50IjpbImltcG9ydCBfIGZyb20gJ2xvZGFzaCc7XG5cbmZ1bmN0aW9uIGNvbXBvbmVudCgpIHtcblx0Y29uc3QgZWxlbWVudCA9IGRvY3VtZW50LmNyZWF0ZUVsZW1lbnQoJ2RpdicpO1xuXG5cdC8vIExvZGFzaCwgbm93IGltcG9ydGVkIGJ5IHRoaXMgc2NyaXB0XG5cdGVsZW1lbnQuaW5uZXJIVE1MID0gXy5qb2luKFsnSGVsbG8nLCAnd2VicGFjayddLCAnICcpO1xuXG5cdHJldHVybiBlbGVtZW50O1xufVxuIl0sInNvdXJjZVJvb3QiOiIifQ==
--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-async function getComponent() {
+import _ from 'lodash';
+
+function component() {
 	const element = document.createElement('div');
-	const { default: _ } = await import('lodash');
+
+	// Lodash, now imported by this script
 	element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+
 	return element;
 }
-
-getComponent().then((component) => {
-	document.body.appendChild(component);
-});
